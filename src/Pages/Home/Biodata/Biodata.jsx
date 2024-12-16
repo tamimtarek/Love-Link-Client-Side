14bb60993f5114638269da19c5511c1e8e0f092c
tamimtarik <tamimtarik15@gmail.com>
2024-12-16 23:00:30 +0600

    update node module

--- src/Pages/Home/Biodata/Biodata.jsx
+++ src/Pages/Home/Biodata/Biodata.jsx
@@ -39,6 +39,22 @@ const Biodata = ({ bio }) => {
             View Profile
           </button>
         </Link>
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Click
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
